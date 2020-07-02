0198383d05a41c66ddc77d3e15d817914a5f6c27
resparx <ramprash.rspx@gmail.com>
2020-07-02 19:15:53 +0530

    array slice

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 	var a int = 1
 	var b = 2
 	c := "detox"
+
+	arr := [...]int{1, 2, 3, 4, 5, 6, 6, 7, 8}
+	slArr := arr[3:5]
+	fmt.Println("arr", arr)
+	fmt.Println("slArr", slArr)
 	fmt.Println("variables are", a, b, c)
 	addd := add.Add(2, 3)
 
